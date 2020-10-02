74fbb3edfa2501d5761e91b9eb7b783c867032fb
lizstangle <lizstangle@me.com>
2020-10-01 21:28:45 -0700

    Update main.py

--- main.py
+++ main.py
@@ -3,23 +3,41 @@ def get_file_lines(filename):
     lines_list = infile.readlines()
     infile.close()
     return lines_list  
-#     or 
-#     for line in infile:
-#         print(line)
-#     result = 
 
-# def lines_printed_backwards(lines_list):  
-#     print(lines_list).reverse
+    # print(get_file_lines("poem.txt"))
 
-# from random import choice
-# def lines_printed_random(lines_list):
-#     number_lines = len(poem)
-#     random.randint(0, len(locations))
+def lines_printed_backwards(lines_list):  
+    lines_list.reverse()
+    lines_length = len(lines_list)
+    for i in range(lines_list):
+        line = lines_list[i]
+        line_number = lines_length - i
+        print(f"{line_number} {line}")
 
 
-#     def lines_
 
-# def lines_printed_customer(lines_list)
+
+
+#     lines_printed_backwards(lines_list):     
+#     lines_list.reverse()     lines_length = len(lines_list)     for i in range(lines_list):         line = lines_list[i]         line_number = lines_length - i         print(f"{line_number} {line}") 
+
+# #     or 
+# #     for line in infile:
+# #         print(line)
+# #     result = 
+
+# # def lines_printed_backwards(lines_list):  
+# #     print(lines_list).reverse
+
+# # from random import choice
+# # def lines_printed_random(lines_list):
+# #     number_lines = len(poem)
+# #     random.randint(0, len(locations))
+
+
+# #     def lines_
+
+# # def lines_printed_customer(lines_list)
 
 
 
