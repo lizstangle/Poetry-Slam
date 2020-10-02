f0a045f227f48718597192f00795696f2dd8a7dc
lizstangle <lizstangle@me.com>
2020-10-01 20:30:10 -0700

    "lines_list"

---
+++ main.py
@@ -0,0 +1,33 @@
+def get_file_lines(filename):
+    infile = open(filename, "r")
+    lines_list = infile.readlines()
+    infile.close()
+    return lines_list  
+#     or 
+#     for line in infile:
+#         print(line)
+#     result = 
+
+# def lines_printed_backwards(lines_list):  
+#     print(lines_list).reverse
+
+# from random import choice
+# def lines_printed_random(lines_list):
+#     number_lines = len(poem)
+#     random.randint(0, len(locations))
+
+
+#     def lines_
+
+# def lines_printed_customer(lines_list)
+
+
+
+
+# def lines_printed_custom(lines_list):
+#     lines_length = len(lines_list)
+
+#     for i in range(lines_list):
+#         if i % 4 == 0 and i % 2 ==0:
+#             print(lines_length[i])
+    
